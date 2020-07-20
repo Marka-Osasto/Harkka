ca6917afc3649d8ad228477154a9f8903bbf81fe
Jyrki <jotai>
2020-07-20 14:10:06 +0300

    Added classes

---
+++ app/src/main/java/com/example/harkka/Game.java
@@ -0,0 +1,18 @@
+package com.example.harkka;
+
+import java.util.ArrayList;
+
+public abstract class Game {
+    private ArrayList<Player> players = new ArrayList<>();
+
+    public Game() {
+//        players.add();
+    }
+
+}
+class GameDarts extends Game {
+
+}
+class GameKiller extends Game {
+
+}
---
+++ app/src/main/java/com/example/harkka/Player.java
@@ -0,0 +1,18 @@
+package com.example.harkka;
+
+public abstract class Player {
+    private int score;
+
+    protected int getScore() {
+        return score;
+    }
+    protected void setScore() {
+
+    }
+}
+class PlayerDarts extends Player {
+
+}
+class PlayerKiller extends Player {
+
+}
